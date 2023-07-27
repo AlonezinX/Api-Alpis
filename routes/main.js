62e44101fef65b69aecd353c4192b0e9dfb93776
The AloneX Ofc <82893484+AlonezinX@users.noreply.github.com>
2023-07-26 21:36:30 -0300

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -31,7 +31,7 @@ function checkAuth(req, res, next) {
         res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, post-check=0, pre-check=0');
         next();
     } else {
-        req.flash('error_messages', "Please Login to continue !");
+        req.flash('error_messages', "Por favor, faça o login para continuar !");
         res.redirect('/login');
     }
 }
